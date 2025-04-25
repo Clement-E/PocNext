1a63ed88255af78b646e6e8f1ac8eaa96c527e20
Clement Eveillard <clement.evd.dev@gmail.com>
2025-04-25 19:34:10 +0200

    create ticket form init

---
+++ next-project/src/app/features/ticket/components/ticket-create-form.tsx
@@ -0,0 +1,23 @@
+import {createTicket} from "@/app/features/ticket/actions/create-ticket";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {Textarea} from "@/components/ui/textarea";
+
+const TicketCreateForm = () => {
+  
+
+  return (
+    <form className="flex flex-col gap-y-2" action={createTicket}>
+      <Label htmlFor="title">Title</Label>
+      <Input id="title" name="title" type="text"/>
+
+      <Label htmlFor="content">Content</Label>
+      <Textarea id="content" name="content" />
+
+      <Button type="submit">Create</Button>
+    </form>
+  )
+}
+
+export { TicketCreateForm }
--- next-project/src/app/tickets/page.tsx
+++ next-project/src/app/tickets/page.tsx
@@ -1,7 +1,9 @@
 import React, {Suspense} from "react";
+import {TicketCreateForm} from "@/app/features/ticket/components/ticket-create-form";
 import {TicketList} from "@/app/features/ticket/components/ticket-list";
 import {Heading} from "@/components/Heading";
 import {Spinner} from "@/components/Spinner";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 
 // to make this page dynamically rendered
 // export const dynamic = "force-dynamic";
@@ -10,14 +12,27 @@ import {Spinner} from "@/components/Spinner";
 
 const TicketsPage = () => {
 
-    return (
-        <div className="flex flex-col flex-1 gap-y-8">
-            <Heading title="Tickets" description="All your tickets in one place" />
-            <Suspense fallback={<Spinner />}>
-                <TicketList />
-            </Suspense>
-        </div>
-    );
+  return (
+    <div className="flex flex-col flex-1 gap-y-8">
+      <Heading title="Tickets" description="All your tickets in one place"/>
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle>
+            <CardTitle>Create ticket</CardTitle>
+            <CardDescription>A new ticket will be created</CardDescription>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <TicketCreateForm />
+        </CardContent>
+      </Card>
+
+      <Suspense fallback={<Spinner/>}>
+        <TicketList/>
+      </Suspense>
+    </div>
+  );
 }
 
 export default TicketsPage;
