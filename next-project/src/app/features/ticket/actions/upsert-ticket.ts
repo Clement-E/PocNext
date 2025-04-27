1647f005e2b325a3361a02254c7bcfe70fee0606
Clement Eveillard <clement.evd.dev@gmail.com>
2025-04-27 16:55:34 +0200

    improves form UX

--- next-project/src/app/features/ticket/actions/upsert-ticket.ts
+++ next-project/src/app/features/ticket/actions/upsert-ticket.ts
@@ -2,25 +2,46 @@
 
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+import {z} from "zod";
 import {prisma} from "@/lib/prisma";
 import {ticketPath, ticketsPath} from "@/path";
 
-export const upsertTicket = async (id: string | null, formData: FormData) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+})
+
+export const upsertTicket = async (
+  id: string | undefined,
+  _actionState: { message : string, payload?: FormData},
+  formData: FormData
+) => {
+
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title"),
+      content: formData.get("content"),
+    })
+
+    await prisma.ticket.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data
+    })
+  } catch (error) {
+    return {
+      message: "shit happened",
+      payload: formData
+    }
   }
-  await prisma.ticket.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data
-  })
 
   // on demand caching
   revalidatePath(ticketsPath());
   if (id) {
     redirect(ticketPath(id))
   }
+
+  return { message: "Ticket created"}
 }
--- next-project/src/app/features/ticket/components/ticket-upsert-form.tsx
+++ next-project/src/app/features/ticket/components/ticket-upsert-form.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import {Ticket} from "@prisma/client";
-import {Button} from "@/components/ui/button";
+import {useActionState} from "react";
+import {SubmitButton} from "@/components/form/submit-button";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
 import {Textarea} from "@/components/ui/textarea";
-import { upsertTicket } from "../actions/upsert-ticket"
+import {upsertTicket} from "../actions/upsert-ticket";
 
 type TicketUpsertFormProps = {
   ticket?: Ticket;
@@ -11,15 +14,19 @@ type TicketUpsertFormProps = {
 
 const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {
 
+  const [actionState, action] = useActionState(upsertTicket.bind(null, ticket?.id), {message: ""})
+
   return (
-    <form className="flex flex-col gap-y-2" action={upsertTicket.bind(null, ticket?.id)}>
+    <form className="flex flex-col gap-y-2" action={action}>
       <Label htmlFor="title">Title</Label>
-      <Input id="title" name="title" type="text" defaultValue={ticket?.title}/>
+      <Input id="title" name="title" type="text" defaultValue={(actionState.payload?.get('title') as string) ?? ticket?.title}/>
 
       <Label htmlFor="content">Content</Label>
-      <Textarea id="content" name="content" defaultValue={ticket?.content}/>
+      <Textarea id="content" name="content" defaultValue={(actionState.payload?.get('content') as string) ?? ticket?.content}/>
 
-      <Button type="submit">{ticket ? "Edit" : "Create"}</Button>
+      <SubmitButton label={ticket ? "Edit" : "Create"}/>
+
+      {actionState.message }
     </form>
   )
 }
