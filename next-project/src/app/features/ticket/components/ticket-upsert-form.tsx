048d577d5511aab0cb58594ec22173c203f9a03d
Clément <clement.evd.dev@gmail.com>
2025-06-07 16:14:36 +0200

    adds a datepicker and a currency field

--- next-project/src/app/features/ticket/components/ticket-upsert-form.tsx
+++ next-project/src/app/features/ticket/components/ticket-upsert-form.tsx
@@ -6,9 +6,11 @@ import {FieldError} from "@/components/form/field-error";
 import { Form } from "@/components/form/form"
 import {SubmitButton} from "@/components/form/submit-button";
 import {EMPTY_ACTION_STATE} from "@/components/form/utils/to-action-state";
+import {DatePicker} from "@/components/ui/date-picker";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
 import {Textarea} from "@/components/ui/textarea";
+import {fromCent} from "@/utils/currency";
 import {upsertTicket} from "../actions/upsert-ticket";
 
 type TicketUpsertFormProps = {
@@ -27,6 +29,34 @@ const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" defaultValue={(actionState.payload?.get('content') as string) ?? ticket?.content}/>
       <FieldError actionState={actionState} name="content" />
+        <div className="flex gap-x-2 mb-1">
+            <div className="w-1/2">
+                <Label htmlFor="deadline">Deadline</Label>
+                <DatePicker
+                    id="deadline"
+                    name="deadline"
+                    defaultValue={
+                        (actionState.payload?.get("deadline") as string) ??
+                        ticket?.deadline
+                    }
+                />
+                <FieldError actionState={actionState} name="deadline" />
+            </div>
+            <div className="w-1/2">
+                <Label htmlFor="bounty">Bounty ($)</Label>
+                <Input
+                    id="bounty"
+                    name="bounty"
+                    type="number"
+                    step=".01"
+                    defaultValue={
+                        (actionState.payload?.get("bounty") as string) ??
+                        (ticket?.bounty ? fromCent(ticket?.bounty) : "")
+                    }
+                />
+                <FieldError actionState={actionState} name="bounty" />
+            </div>
+        </div>
       <SubmitButton label={ticket ? "Edit" : "Create"}/>
     </Form>
   )
